e637ab9567ed010eb0def0b7fee5dde9536efd76
Ryan Smith <ryan.l.smith142@gmail.com>
2021-01-27 18:53:46 -0600

    Adds more fields to ServiceDog object

--- models/ServiceDog.js
+++ models/ServiceDog.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const ServiceDogSchema = new mongoose.Schema({
     name: String,
+    handler: String,
+    certifyingOrganization: String,
+    dateLastCertified: Number,
     qrCode: String
 });
 
--- routes/api/serviceDog.js
+++ routes/api/serviceDog.js
@@ -13,26 +13,6 @@ router.post('/', async function (req, response) {
 
 router.get('/', function (req, res) {
     
-    var options = {
-            "frame_name": "no-frame",
-            "qr_code_text": "https://www.qr-code-generator.com/",
-            "image_format": "SVG",
-            "background_color": "#ffffff",
-            "foreground_color": "#fa6e79",
-            "marker_right_inner_color": "#2d7cda",
-            "marker_right_outer_color": "#00bfff",
-            "marker_left_template": "version13",
-            "marker_right_template": "version13",
-            "marker_bottom_template": "version13"
-        
-    };
-      
-      function callback(error, response, body) {
-          res.json(response);
-      }
-      
-      request(options, callback);
-    
     
     res.end();
 })
