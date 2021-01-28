77c1c73f55e2bc61fc93e607d4b89cba19e206d0
Ryan Smith <ryan.l.smith142@gmail.com>
2021-01-27 18:53:46 -0600

    Adds request package and makes call to github repo

--- routes/api/serviceDog.js
+++ routes/api/serviceDog.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 var http = require("https");
 const ServiceDog = require('../../models/ServiceDog');
+const request = require('request');
 
 router.post('/', async function (request, response) {
 
@@ -10,8 +11,24 @@ router.post('/', async function (request, response) {
     response.json(newServiceDog);
 });
 
-router.get('http://api.qrserver.com/v1/create-qr-code/?data=HelloWorld!&size=100x100', async function(request, response) {
-    response.json(response);
+router.get('/', function (request, response) {
+    
+
 })
+
+const options = {
+    url: 'https://api.github.com/repos/request/request',
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+  
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log("hey there");
+    }
+  }
+  
+  request(options, callback);
 
 module.exports = router;
