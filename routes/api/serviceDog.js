e58ac49b0dc23f3ddcfe3f575e8329ac5ecf32d6
Ryan Smith <ryan.l.smith142@gmail.com>
2021-01-29 18:37:45 -0600

    Playing around with request object

--- routes/api/serviceDog.js
+++ routes/api/serviceDog.js
@@ -2,19 +2,25 @@ const express = require('express');
 const router = express.Router();
 const ServiceDog = require('../../models/ServiceDog');
 const request = require('request');
-
+const http = require('http');
 
 router.post('/', async function (req, response) {
+    console.log(req.headers);
 
-    await newServiceDog.save();
+    response.end();
 
-    response.json(newServiceDog);
+    // await newServiceDog.save().then(serviceDog => {
+    //     response.send("Item saved to database " + serviceDog);
+    // });
+
+    // // response.json("Service Dog Profile was successfully created for: ");
 });
 
 router.get('/', function (req, res) {
+
+    console.log(req.headers);
     
-    
-    res.end();
+    res.status(404).end();
 })
 
 module.exports = router;
