ce4749016a5651f856c5f3ed51ea09be618b7a7c
Ryan Smith <ryan.l.smith142@gmail.com>
2021-02-03 19:46:20 -0600

    Adds routes for service dog profiles

--- routes/api/serviceDog.js
+++ routes/api/serviceDog.js
@@ -2,25 +2,24 @@ const express = require('express');
 const router = express.Router();
 const ServiceDog = require('../../models/ServiceDog');
 const request = require('request');
-const http = require('http');
 
-router.post('/', async function (req, response) {
-    console.log(req.headers);
 
-    response.end();
 
-    // await newServiceDog.save().then(serviceDog => {
-    //     response.send("Item saved to database " + serviceDog);
-    // });
-
-    // // response.json("Service Dog Profile was successfully created for: ");
+router.post('/', function (req, res) {
+    res.json("Creating or updating a service dog profiles.")
 });
 
 router.get('/', function (req, res) {
+    res.json("Getting a service dog profile.");
 
-    console.log(req.headers);
-    
-    res.status(404).end();
+});
+
+router.get('/getAllServiceDogs', function (req, res) {
+    res.json("Getting all service dog profiles.");
+});
+
+router.delete('/', function (req, res) {
+    res.json("Delete a service dog profile.")
 })
 
 module.exports = router;
